9d187ff59a04e670ea4b33fa1cc704e4bc93977b
devlyn37 <dev37-2009@hotmail.com>
2021-10-23 21:35:29 -0700

    Completed section 1 part 4 of the project.

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -2,9 +2,14 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./libraries/Base64.sol";
+
 import "hardhat/console.sol";
 
-contract MyEpicGame {
+contract MyEpicGame is ERC721 {
     struct CharacterAttributes {
         uint256 characterIndex;
         string name;
@@ -13,9 +18,22 @@ contract MyEpicGame {
         uint256 maxHp;
         uint256 attackDamage;
         uint256 pp;
+        uint256 maxPp;
     }
 
+    // The tokenId is the NFTs unique identifier, it's just a number that goes
+    // 0, 1, 2, 3, etc.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     CharacterAttributes[] defaultCharacters;
+
+    // We create a mapping from the nft's tokenId => that NFTs attributes.
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // A mapping from an address => the NFTs tokenId. Gives me an ez way
+    // to store the owner of the NFT and reference it later.
+    mapping(address => uint256) public nftHolders;
 
     constructor(
         string[] memory characterNames,
@@ -23,7 +41,7 @@ contract MyEpicGame {
         uint256[] memory characterHp,
         uint256[] memory characterAttackDmg,
         uint256[] memory characterPp
-    ) {
+    ) ERC721("Pokes", "POKE") {
         for (uint256 i = 0; i < characterNames.length; i += 1) {
             defaultCharacters.push(
                 CharacterAttributes({
@@ -33,7 +51,8 @@ contract MyEpicGame {
                     hp: characterHp[i],
                     maxHp: characterHp[i],
                     attackDamage: characterAttackDmg[i],
-                    pp: characterPp[i]
+                    pp: characterPp[i],
+                    maxPp: characterPp[i]
                 })
             );
 
@@ -45,5 +64,86 @@ contract MyEpicGame {
                 c.imageURI
             );
         }
+
+        _tokenIds.increment();
+    }
+
+    function mintCharacterNFT(uint256 _characterIndex) external {
+        // (starts at 1 since we incremented in the constructor).
+        uint256 newItemId = _tokenIds.current();
+
+        // Mint function from ERC721
+        _safeMint(msg.sender, newItemId);
+
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].hp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage,
+            pp: defaultCharacters[_characterIndex].pp,
+            maxPp: defaultCharacters[_characterIndex].pp
+        });
+
+        console.log(
+            "Minted NFT w/ tokenId %s and characterIndex %s",
+            newItemId,
+            _characterIndex
+        );
+
+        nftHolders[msg.sender] = newItemId;
+        _tokenIds.increment();
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[
+            _tokenId
+        ];
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(
+            charAttributes.attackDamage
+        );
+        string memory strPp = Strings.toString(charAttributes.pp);
+        string memory strMaxPp = Strings.toString(charAttributes.maxPp);
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        charAttributes.name,
+                        " -- NFT #: ",
+                        Strings.toString(_tokenId),
+                        '", "description": "This is an NFT that lets people play Pokemon!", "image": "',
+                        charAttributes.imageURI,
+                        '", "attributes": [ { "trait_type": "Health Points", "value": ',
+                        strHp,
+                        ', "max_value":',
+                        strMaxHp,
+                        '}, { "trait_type": "Attack Damage", "value": ',
+                        strAttackDamage,
+                        '}, { "trait_type": "Power Points", "value": ',
+                        strPp,
+                        ', "max_value":',
+                        strMaxPp,
+                        "} ]}"
+                    )
+                )
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        return output;
     }
 }
